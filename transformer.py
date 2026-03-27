23e348597c95f1baa69cb6e577587f26875bfdb6
AtharvaFTW <atharvavankhede@gmail.com>
2026-03-27 16:22:57 +0530

    Multi Head Attention

--- transformer.py
+++ transformer.py
@@ -3,6 +3,9 @@ from torch import nn
 import numpy as np
 
 class MultiHeadAttention(nn.Module):
+    """
+    Implementation of MultiHeadAttention block from the Attention Is All You Need.
+    """
     def __init__(self,d_model: int = 512 , h: int = 8):
         super().__init__()
         
@@ -24,13 +27,13 @@ class MultiHeadAttention(nn.Module):
         K = K.view(batch, seq_len, self.h, self.d_k)
         V = V.view(batch, seq_len, self.h, self.d_k)
 
-        Q.transpose(1,2)
-        K.transpose(1,2)
-        V.transpose(1,2)
+        Q = Q.transpose(1,2)
+        K = K.transpose(1,2)
+        V = V.transpose(1,2)
 
         scores = torch.softmax(torch.matmul(Q,K.transpose(-2, -1))/torch.sqrt(torch.tensor(self.d_k, dtype = torch.float32)), dim = -1)
         attention = torch.matmul(scores , V)
-        attention = attention.transpose(1,2)
+        attention = attention.transpose(1,2).contiguous()
         attention = attention.view(batch, seq_len, d_model)
 
         multi_head = self.W_O(attention)
